746b0c7c60c4aeae818317f322ed0bbc9d1aa7e5
lakshya kumar pal <lakshyapal028@GMAIL.COM>
2018-08-28 12:10:02 +0530

    Add files via upload

---
+++ model-component/script.js
@@ -0,0 +1,13 @@
+$(document).ready(()=>{
+	
+	$('#openModel').click(()=>{
+            $('.modal').css('display','block');            
+    });
+
+    $('.close').click(()=>{
+            $('.modal').css('display','none');
+    });
+    
+});
+
+        
